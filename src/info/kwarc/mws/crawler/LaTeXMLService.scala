084287793b5e85c300671e488ec0ebafca944fd7
alexglont <alex_glontzaru@yahoo.com>
2016-06-30 18:26:29 +0200

    Post the data to latexml

--- src/info/kwarc/mws/crawler/LaTeXMLService.scala
+++ src/info/kwarc/mws/crawler/LaTeXMLService.scala
@@ -2,7 +2,35 @@ package info.kwarc.mws.crawler
 
 import java.net._
 import scala.xml._
+import java.io._
+import org.apache.commons._
+import org.apache.http._
+import org.apache.http.client._
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client._
+import java.util.ArrayList
+import org.apache.http.message.BasicNameValuePair
+import org.apache.http.client.entity.UrlEncodedFormEntity
 
 class LaTeXMLService(url : URL) {
-  def convert(latex : String) : Node = ??? 
+  def convert(profile : String, latex : String) : String = {
+    
+    val post = new HttpPost(url.toString());
+    
+    val nameValuePairs = new ArrayList[NameValuePair](1)
+    nameValuePairs.add(new BasicNameValuePair("profile", profile));
+    nameValuePairs.add(new BasicNameValuePair("tex", latex));
+    
+    post.setEntity(new UrlEncodedFormEntity(nameValuePairs));    
+    
+    val client = new DefaultHttpClient(); 
+    
+    println(post.getRequestLine());
+    
+    val response = client.execute(post);
+    
+    println(response.getEntity());
+    
+    return response.toString();
+  }
 }
--- src/info/kwarc/mws/crawler/Main.scala
+++ src/info/kwarc/mws/crawler/Main.scala
@@ -1,7 +1,14 @@
 package info.kwarc.mws.crawler
 
+import java.net._
+
 object Main {
   def main(args : Array[String]) {
+    
+      val url = new URL("http://latexml.mathweb.org/convert");
+      val latexmlService = new LaTeXMLService(url);
+      
+      println(latexmlService.convert("12", "$$E=mc^2$$"));
     
   }
   
